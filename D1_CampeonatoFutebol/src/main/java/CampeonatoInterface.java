89fbf59b7f93c3ff2cdf35f0943598e9f701c74c
Nicoli Kassa <151332561+Nicoli-Kassa@users.noreply.github.com>
2025-04-20 14:09:29 -0300

    Delete D1_CampeonatoFutebol directory

--- D1_CampeonatoFutebol/src/main/java/CampeonatoInterface.java
+++
@@ -1,7 +0,0 @@
-
-public interface CampeonatoInterface {
-    void cadastrarTime(String nome, String cidade);
-    void cadastrarJogadora(String nomeJogadora, int idade, String posicao, int numeroCamisa, String nomeTime);
-    void registrarPartida(String time1, String time2, int golsTime1, int golsTime2);
-    void visualizarTabela();
-}
